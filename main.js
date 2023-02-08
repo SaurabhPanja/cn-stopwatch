0768240e49279f8108c93a1d73d7685bd5cf0f10
Saurabh Panja <saurabh.panja@phamax.ch>
2023-02-09 00:31:58 +0530

    finished styling

--- main.js
+++ main.js
@@ -2,20 +2,27 @@
   let milliSeconds = 0;
   let seconds = 0;
   let stopCounterToClearInterval = 1;
+  let isRunning = false;
 
   document.querySelector("#start-btn").addEventListener("click", () => {
+    if (isRunning) return;
+    isRunning = true;
     stopCounterToClearInterval = setInterval(() => {
+      const milliSecondsBy100 = milliSeconds % 100
       document.querySelector("#display-milliseconds").innerHTML =
-        milliSeconds % 100;
+        milliSecondsBy100 < 10 ? "0" + milliSecondsBy100 : milliSecondsBy100;
+
       seconds = Math.floor(milliSeconds / 100);
       document.querySelector("#display-seconds").innerHTML =
         seconds < 10 ? "0" + seconds : seconds;
+        
       milliSeconds++;
     }, 10);
   });
 
   document.querySelector("#stop-btn").addEventListener("click", () => {
     clearInterval(stopCounterToClearInterval);
+    isRunning = false;
   });
 
   document.querySelector("#reset-btn").addEventListener("click", () => {
@@ -25,5 +32,7 @@
 
     document.querySelector("#display-milliseconds").innerHTML = "00";
     document.querySelector("#display-seconds").innerHTML = "00";
+
+    isRunning = false;
   });
 })();
